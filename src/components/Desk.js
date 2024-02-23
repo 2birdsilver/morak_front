df6194523c0f7660973f8d5b09b2fac23d5065ef
youngeun <westzerosilver@gmail.com>
2024-02-23 10:28:29 +0900

    Design: 메인화면 css 적용

--- src/components/Desk.js
+++ src/components/Desk.js
@@ -1,23 +1,17 @@
-// src/components/Desk.js
-
 import React from 'react';
-import '../App.css'; // 혹은 적절한 CSS 파일 경로
+// import '../App.css'; // 혹은 적절한 CSS 파일 경로
 
 function Desk({ member, onClick }) {
   return (
-    <div className='member'>
-    <div className='photo'>
+    <div className='member' onClick={onClick}>
       {member.photoUrl ? (
-        <img src={member.photoUrl} alt={`${member.name}'s photo`} />
+        <img src={member.photoUrl} className='photo' alt={`${member.name}'s photo`} />
       ) : (
         <div>사진 없음</div>
       )}
-    </div> 
       <div className='intern'>
         <div className='intern_name'>{member.name}</div>
         <div className='intern_intro'>{member.introduction}</div>
-        <button onClick={onClick}>메모 보기</button>
-
       </div>
     </div>
   );
