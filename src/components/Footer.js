0e3b5064f20179471233189e38d7816689547470
youngeun <westzerosilver@gmail.com>
2024-02-23 17:45:55 +0900

    design: 모바일 반응형

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-        <div className='text'>by묻다나믿</div>
+        <div className='text'>© 2024 by 묻다나믿. All rights reserved.</div>
+        <div className='text'>이메일: leesu@kcc.co.kr</div>
+        <div className='text'>주소: 서울 용산구 청파로61길 5, 6층</div>
     </div>
   )
 }
