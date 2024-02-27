1b82ddb1f5ceb7674931b640a5332390378a218a
youngeun <westzerosilver@gmail.com>
2024-02-27 14:03:59 +0900

    design: 하트 메모 반응형 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className='header'>
         <div className='logo'>KCC정보통신</div>
-        <div className='title'>Happy Desk</div>
+        <div className='title'>KCC's Happy Desk</div>
         <div className='text'>포스트잇으로 메모를 남겨보세요!</div>
     </div>
   )
