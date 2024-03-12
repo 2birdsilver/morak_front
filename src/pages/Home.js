002d4c7f7445bcf6286d2036fd307e3ef881fe76
2birdsilver <seeun0426@gmail.com>
2024-03-12 17:48:22 +0900

    feat: 으앙 드디어 로그인 성공 시 access_token을 받아서 local storeage에 저장 완료!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Desk from '../components/Desk.js';
 import { useNavigate, useLocation } from 'react-router-dom';
 
+
 function Home() {
   const [members, setMembers] = useState([]);
   const navigate = useNavigate();
-  const location = useLocation(); // useLocation 훅 사용
+  const location = useLocation();
 
   const goToMemos = (member) => {
     navigate(`/memo/${member.id}`);
@@ -18,16 +19,14 @@ function Home() {
       .catch((error) => console.error("Fetching members failed", error));
 
     const token = searchParam('token');
-
     if (token) {
       localStorage.setItem("access_token", token);
-      console.log("token: " + token);
     }
 
     function searchParam(key) {
-      return new URLSearchParams(location.search).get(key);
+      return new URLSearchParams(window.location.search).get(key);
     }
-  }, []);
+  }, [location]);
 
   return (
     <div className="wrap">
