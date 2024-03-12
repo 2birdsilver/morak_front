2a25b470af0ef39e8dd88eddc4957a202f5711e8
2birdsilver <seeun0426@gmail.com>
2024-03-12 15:45:42 +0900

    fix: 네이버 로그인 기능 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import Desk from '../components/Desk.js';
-import { useNavigate  } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function Home() {
-    const [members, setMembers] = useState([]);
-    const navigate = useNavigate();
+  const [members, setMembers] = useState([]);
+  const navigate = useNavigate();
+  const location = useLocation(); // useLocation 훅 사용
 
-    const goToMemos = (member) => {
-      navigate(`/memo/${member.id}`);
-    };
+  const goToMemos = (member) => {
+    navigate(`/memo/${member.id}`);
+  };
 
-    useEffect(() => {
-      fetch('/members')
-        .then((response) => response.json())
-        .then((data) => setMembers(data))
-        .catch((error) => console.error("Fetching members failed", error));
-    }, []);
+  useEffect(() => {
+    fetch('/members')
+      .then((response) => response.json())
+      .then((data) => setMembers(data))
+      .catch((error) => console.error("Fetching members failed", error));
 
-    return (
-      <div className="wrap">
-        <div className='container'>
-          {members.map((member) => (
-            <Desk key={member.id} member={member} onClick={() => goToMemos(member)} />
-            ))}
-        </div>
+    const token = searchParam('token');
+
+    if (token) {
+      localStorage.setItem("access_token", token);
+      console.log("token: " + token);
+    }
+
+    function searchParam(key) {
+      return new URLSearchParams(location.search).get(key);
+    }
+  }, []);
+
+  return (
+    <div className="wrap">
+      <div className='container'>
+        {members.map((member) => (
+          <Desk key={member.id} member={member} onClick={() => goToMemos(member)} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-export default Home
+export default Home;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
+import naverloginbtn from '../images/naverloginbtn.png';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -9,19 +10,19 @@ function Login() {
   const navigate = useNavigate();
 
   const handleLoginSubmit = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     try {
       const res = await axios.post('http://localhost:8080/auth/login', {
         email,
         password,
       });
-      alert("로그인 성공"); 
+      alert("로그인 성공");
       localStorage.clear();
       localStorage.setItem("id", res.data.id)
       localStorage.setItem("name", res.data.name);
-      localStorage.setItem("url",res.data.avatarUrl);
-       // 사용자 정의 이벤트 발생
-       window.dispatchEvent(new Event("loginSuccess"));
+      localStorage.setItem("url", res.data.avatarUrl);
+      // 사용자 정의 이벤트 발생
+      window.dispatchEvent(new Event("loginSuccess"));
       navigate('/');
     } catch (error) {
       alert('로그인 실패'); // 에러 처리
@@ -30,41 +31,41 @@ function Login() {
 
   return (
     <div className='wrap memo'>
-        <div className='login-form-container'>
+      <div className='login-form-container'>
         <h1>Login</h1>
-            <form className="login-form" onSubmit={handleLoginSubmit}>
-                <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    placeholder='이메일을 입력하세요'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder='비밀번호를 입력하세요'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <div className='login-btns'>
-                    <button type="submit" className='login-btn'>로그인</button>
-                    <button type="submit" className='sign-btn'>회원가입</button>
-                    <div className='hr-line'>
-                        <hr />
-                        <span>OR</span>
-                        <hr/>
-                    </div>
-                    <button type="submit" className='sign-btn google-login'>
-                        <i className='icon-google'></i>
-                        구글 계정으로 로그인
-                    </button>
-                </div>
-               
-            </form>
-        </div>
+        <form className="login-form" onSubmit={handleLoginSubmit}>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder='이메일을 입력하세요'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder='비밀번호를 입력하세요'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className='login-btns'>
+            <button type="submit" className='login-btn'>로그인</button>
+            <button type="submit" className='sign-btn'>회원가입</button>
+            <div className='hr-line'>
+              <hr />
+              <span>OR</span>
+              <hr />
+            </div>
+
+          </div>
+
+        </form>
+        <a href="http://localhost:8080/oauth2/authorization/naver">
+        <img className='naverloginbtn' src={naverloginbtn} alt="naverloginbtn" />
+        </a>
+      </div>
     </div>
   )
 }
