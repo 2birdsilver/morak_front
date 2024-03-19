2c80618a3ca134af58c1b63f109eca1d386fa8fe
youngeun <westzerosilver@gmail.com>
2024-03-19 17:24:57 +0900

    fix: 자기소개 글 더보기 추가

--- src/components/Desk.js
+++ src/components/Desk.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+// 팝업 모달 컴포넌트
+function Modal({ content, name, onClose }) {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <p>{name}</p>
+        <p>{content}</p>
+        <button onClick={onClose}>닫기</button>
+      </div>
+    </div>
+  );
+}
 
 //props로 member 객체와 onClick 함수를 받음
 function Desk({ member, onClick }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // "더보기" 클릭 핸들러
+    const handleReadMoreClick = (e) => {
+      e.stopPropagation(); // Desk의 onClick 이벤트가 발생하지 않도록 방지
+      setIsModalOpen(true);
+    };
+  
+    // 모달 닫기 핸들러
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
+    };
+
   return (
     <div className='member' onClick={onClick}>
       {member.photoUrl ? (
@@ -11,8 +37,20 @@ function Desk({ member, onClick }) {
       )}
       <div className='intern'>
         <div className='intern_name'>{member.name}</div>
-        <div className='intern_intro'>{member.introduction}</div>
+        <div className='intern_intro'>
+        {member.introduction.length > 20 ? (
+            <>
+              <div>{member.introduction.substring(0, 20)}... 
+                <button onClick={handleReadMoreClick} className='intern__btn-more'>더보기</button>
+              </div>
+             
+            </>
+          ) : (
+            member.introduction
+          )}
+        </div>
       </div>
+      {isModalOpen && <Modal content={member.introduction} name={member.name} onClose={handleCloseModal} />}
     </div>
   );
 }
