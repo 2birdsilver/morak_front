7e9043ce5c6cb9fd67a5065ba5f68f0a9daf56f3
이세은 <seeun0426@gmail.com>
2025-11-07 20:15:02 +0900

    chore: 머지 전 커밋

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,12 +13,6 @@ function Header() {
     // 현재 페이지 경로 확인
     const path = location.pathname;
 
-    // signup 페이지라면 redirect 로직 무시
-    if (path.includes("signup")) {
-      window.location.href = "/#/signup";
-      return;
-    }
-
     // 쿼리 파라미터로 받은 access token을 local storage에 저장
     const token = searchParam("token");
     if (token) {
@@ -41,13 +35,19 @@ function Header() {
       }
     };
 
+    // signup 페이지라면 redirect 로직 무시
+    if (path.includes("signup")) {
+      window.location.href = "/#/signup";
+      return;
+    }
+
     updateProfile(); // 컴포넌트 마운트 시 호출
     window.addEventListener("loginSuccess", updateProfile); // 사용자 정의 이벤트 리스너 추가
 
     return () => {
       window.removeEventListener("loginSuccess", updateProfile); // 컴포넌트 언마운트 시 리스너 제거
     };
-  }, [location, getUserInfo]);
+  }, [currentUser]);
 
   const goToHome = () => {
     navigate("/");
