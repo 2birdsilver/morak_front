70ba5aac69cb659dd986889cd1e6fb6b44926f7a
이세은 <seeun0426@gmail.com>
2026-02-04 23:42:15 +0900

    feat: 메인화면에 방명록 섹션별 리스트 ui설정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,22 @@ import MemoList from "../components/MemoList.js";
 import { useNavigate } from "react-router-dom";
 import memoApi, { getMainMemos } from "../api/memoApi.js";
 import SearchBox from "../components/SearchBox.js";
+import {
+  Section,
+  HorizontalGuestbookList,
+  Guestbook,
+} from "../components/Guestbook";
 
 function Home() {
   const [members, setMembers] = useState([]);
   const [memos, setMemos] = useState([]);
   const navigate = useNavigate();
   const [searchKeyword, setSearchKeyword] = useState("");
+
+  const sections = [
+    { type: "LATEST", title: "최신순" },
+    { type: "POPULAR", title: "인기순" },
+  ];
 
   // const goToMemos = (memo) => {
   //   navigate(`/memo/${member.id}`);
@@ -52,8 +62,16 @@ function Home() {
           <SearchBox onSearch={handleSearch} />
         </div>
 
-        {/* 메모리스트 */}
-        <MemoList memoList={memos} />
+        {/* 방명록리스트 */}
+        <div>
+          {sections.map((section) => (
+            <Section
+              key={section.type}
+              type={section.type}
+              title={section.title}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
